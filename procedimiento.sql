424d34eeeae4b63914125fa05c53f7e89530857e
Iván Darío Villamizar <56414770+ivillamizar5@users.noreply.github.com>
2025-05-20 22:18:21 -0500

    Update procedimiento.sql

--- procedimiento.sql
+++ procedimiento.sql
@@ -1,9 +1,4 @@
 
-
-Pasos a desarrollar: Crear base de datos de datos alquiler peliculas en ella debes abrir un archivo sql donde copiaras los datos del archivo db.sql, luego insertaras los datos con la informacion que se encuentra en el archivo insert.sql. Las consultas se encuentran  en el archivo consultas.sql por ultimo el diagrama realcional se encuentra como imagen er.png
-
- 
- 
  create or replace function registrarcliente(cliente int)
  return text as $$
 begin
